686d9b9fbc93fbe0a6fb2de805b6718a00a3f0d7
Sergey <ext.sergey.kuchin@grabtaxi.com>
2019-07-13 07:02:49 +0800

    corrected mistakes in texts

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -26,10 +26,10 @@ class Bender(
 
         return if (question.answers.contains(answer.toLowerCase(Locale("ru")))) {
             question = question.nextQuestion()
-            "Отлично - это правильный ответ!\n${question.question}" to status.color
+            "Отлично - ты справился\n${question.question}" to status.color
         } else {
             status = status.nextStatus()
-            "Это не правильный ответ\n${question.question}" to status.color
+            "Это неправильный ответ\n${question.question}" to status.color
         }
     }
 
